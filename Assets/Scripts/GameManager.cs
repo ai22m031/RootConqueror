d562928deced0351c3dca9a7605be1e8c4114076
m047 <ai22m047@technikum-wien.at>
2023-02-02 20:41:01 +0100

    Created basic code structure

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    LogicManager lm;
+    MusicManager mm;
+    UIManager uim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(instance == null)
+            instance = this;
+        else
+            throw new Exception("Created another instance of singleton Game Manager");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
