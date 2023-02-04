8e804cf69a37171cc2f1f7c6d0165717af25e5d8
DESKTOP-2T69R5O\Alex <puchingeralexander@gmail.com>
2023-02-04 12:48:09 +0100

    no message

--- Assets/Scripts/SpawnEnemies.cs
+++ Assets/Scripts/SpawnEnemies.cs
@@ -27,7 +27,18 @@ public class SpawnEnemies : MonoBehaviour
     {
         if(Time.time > timeStamp) {
             Bounds convexHullBounds = gm.cm.CalculateBounds(gm.tm.towers,false);
+            Vector3 minCoords = convexHullBounds.min;
+            Vector3 maxCoords = convexHullBounds.max;
             //Insert Logic to spawn Enemy outside of this bounds
+            /*
+             * prepareEnemyPosition = new Vector2(x,y,z);
+             * while(convexHullBounds.Contains(prepareEnemyPosition){
+             *  GameObject enemy = Instantiate(enemyPrefabs[Random.Range(0, enemyPrefabs.Count)], prepareEnemyPosition, Quaternion.identity);
+             * }
+             * Instnatiate Enemy
+             */
+            
+            
             Vector2 spawnPos = new Vector2(spawnCenter.x + spawnRadius * Mathf.Cos(currSpawnAngle), spawnCenter.y + spawnRadius * Mathf.Sin(currSpawnAngle));
             GameObject enemy = Instantiate(enemyPrefabs[Random.Range(0, enemyPrefabs.Count)], spawnPos, Quaternion.identity);
             //GameObject enemy = Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
